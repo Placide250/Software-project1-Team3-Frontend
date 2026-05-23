201884500af5c6c00c7a6af29148018638a69f0f
isaiahpfisher <isaiahpfisher@gmail.com>
2026-05-23 15:24:18 -0500

    ipf: configure frontend deploy action

--- src/services/services.js
+++ src/services/services.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 var baseurl = "";
 if (process.env.NODE_ENV === "development") {
-  baseurl = "http://localhost/recipeapi/";
+  baseurl = "http://localhost/planetapi/";
 } else {
-  baseurl = "/recipeapi/";
+  baseurl = "/planetapi/";
 }
 
 const apiClient = axios.create({
