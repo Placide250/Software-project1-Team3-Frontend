37347bed5680167552d87a330e9f481a0cf7a7be
Placide <placidestana12@gmail.com>
2026-05-31 19:45:30 -0500

    made quick changes on login interface

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -111,7 +111,7 @@ function closeSnackBar() {
             color="secondary"
             @click="navigateToRecipes()"
           >
-            View Published Recipes
+            Browse Events
           </v-btn>
         </v-card-title>
       </v-card>
