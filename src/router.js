61132e60f5a51d879bde329208add1862da6747c
IanGWhite <ian.white@eagles.oc.edu>
2026-06-06 11:05:06 -0500

    Basic layout and creation functionality added

--- src/router.js
+++ src/router.js
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import("./views/RecipeList.vue"),
     },
     {
+      path: "/CreateEvent",
+      name: "createEvent",
+      component: () => import("./views/CreateEvent.vue"),
+    },
+    {
       path: "/recipe/:id",
       name: "editRecipe",
       props: true,
